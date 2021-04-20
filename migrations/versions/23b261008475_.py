2e6ed76be9d38013fd238583af546ec82692dea2
borisbrunop <borisbruno88@gmail.com>
2021-04-20 17:44:35 -0400

    requirements.txt try 8

--- migrations/versions/23b261008475_.py
+++ migrations/versions/23b261008475_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 0b468435dcb8
+Revision ID: 23b261008475
 Revises: 
-Create Date: 2021-04-18 21:39:57.837949
+Create Date: 2021-04-19 00:40:14.754201
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '0b468435dcb8'
+revision = '23b261008475'
 down_revision = None
 branch_labels = None
 depends_on = None
