7d4724730ee3b09b897d40555d9481d2082b5760
borisbrunop <borisbruno88@gmail.com>
2021-04-22 14:57:43 -0400

    change models active default value

--- src/models.py
+++ src/models.py
@@ -7,7 +7,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    active = db.Column(db.Boolean(), unique=False, nullable=False)
+    active = db.Column(db.Boolean(False), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __init__(self, email, password, active):
